9b2b6d264cb7b06082c5bb194d7455bda638e124
Simon Bengtsson <simon@goodsolutions.se>
2015-04-09 18:02:19 +0200

    added downvote button to posts

---
+++ client/views/posts/modules/post_downvote.js
@@ -0,0 +1,21 @@
+Template[getTemplate('postDownvote')].helpers({
+  downvoted: function(){
+    var user = Meteor.user();
+    if(!user) return false;
+    return _.include(this.downvoters, user._id);
+  }
+});
+
+Template[getTemplate('postDownvote')].events({
+  'click .downvote-link': function(e, instance){
+    var post = this;
+    e.preventDefault();
+    if(!Meteor.user()){
+      Router.go('atSignIn');
+      Messages.flash(i18n.t("please_log_in_first"), "info");
+    }
+    Meteor.call('downvotePost', post, function(error, result){
+      trackEvent("post downvoted", {'_id': post._id});
+    });
+  }
+});
